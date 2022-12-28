416e9da1e6df5c226ef8807c8bd16af1d6139015
ASIF BIN ABEDIN <asif@ASIFs-MacBook-Pro.local>
2022-12-29 01:26:07 +0600

    Display, Insertion Done

--- js/main.js
+++ js/main.js
@@ -3,35 +3,46 @@
 // var object = Object.assign({}, ...Object.entries({...keys}).map(([a,b]) => ({ [b]: 'someValue' })))
 
 $(document).ready(function(){
-    
-    const p1 = {Name:"Jhon", Phone:23950, Address:"white"};
-    const p2 = {Name:"Alex", Phone:2345238, Address:"black"};
-    const phoneBook = [];
-    phoneBook.push(p1);
-    phoneBook.push(p2);
 
-    
+    const phoneBook = [];
+
+    function dispaly(){
+        var html ="";
+            html+='<tr>';
+            html+='<th>Name</th>';
+            html+='<th>Surname</th>';
+            html+='<th>Phone</th>';
+            html+='<th>Address</th>';
+            html+='<th>Action</th>';
+            html+='</tr>';
+        for(var i = 0; i<phoneBook.length; i++){
+            html+='<tr>';
+            html+='<td>'+phoneBook[i].Name+'</td>';
+            html+='<td>'+phoneBook[i].Surname+'</td>';
+            html+='<td>'+phoneBook[i].Phone+'</td>';
+            html+='<td>'+phoneBook[i].Address+'</td>';
+            html+='<td><button>Update</button> || <button>Delete</button></td>';
+            html+='</tr>';
+        }
+
+        document.getElementById("print").innerHTML = html;
+    }
+    dispaly();
+
+
 
     $('#savebtn').click((ev)=>{
         ev.preventDefault();
         
         let info = {
             Name: $("#firstName").val(),
+            Surname: $("#surname").val(),
             Phone: $("#phone").val(),
             Address: $("#address").val()
         }
-
         phoneBook.push(info);
-        
         $('#inputForm')[0].reset();
-
-        phoneBook.forEach(dispaly);
-        function dispaly(value,index,array){
-             //console.log(value);
-            for (data in value) {
-                console.log(value[data]);
-            }
-        }
+        dispaly();
 
     });
 
